07ac016f9a69a4fac4e82881719fb82d39bf6518
AddyHerrera <addy.herrerapaz@cbtis72.edu.mx>
2022-06-09 12:25:20 -0500

    Termine la carpeta 79 y empecé la carpeta 80, donde llegue hasta el video 11.
    Termine la carpeta 79, donde descargue el AdminLTE, cree algunos templates, realice algunos ajustes al diseño de AdminLTE y cree el menú principal, al igual inicie la carpeta 80 donde llegue hasta el video 11, aquí cree el formulario para crear administradores, cree el password con php, conecte la base de datos desde admin, inserte los administradores, añadí algunas notificaciones, cree el formulario de login, loguie los usuarios php, cree sesiones con php, cree áreas seguras para los usuarios, añadí DataTables para mostrar los registros y cree la tabla en la base de datos para administradores.

---
+++ proyectoconferencias/admin/js/admin-ajax.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    $('#crear-admin').on('submit', function(e) {
+        e.preventDefault();
+
+        var datos = $(this).serializeArray();
+
+        $.ajax({
+            type: $(this).attr('method'),
+            data: datos, 
+            url: $(this).attr('action'),
+            dataType: 'json',
+            success: function(data){
+                var resultado = data;
+                if(resultado.respuesta == 'exito') {
+                    swal(
+                        'Correcto',
+                        'El administrador se creo correctamente',
+                        'success'
+                    )
+                } else {
+                    swal(
+                        'Error!',
+                        'Hubo un error',
+                        'error'
+                    )
+                }
+            }
+        })
+    });
+
+    $('#login-admin').on('submit', function(e) {
+        e.preventDefault();
+
+        var datos = $(this).serializeArray();
+
+        $.ajax({
+            type: $(this).attr('method'),
+            data: datos, 
+            url: $(this).attr('action'),
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+                var resultado = data;
+                if(resultado.respuesta == 'exitoso') {
+                    swal(
+                        'Login Correcto',
+                        'Bienvenid@ '+resultado.usuario+' !! ',
+                        'success'
+                    )
+                    setTimeout(function(){
+                        window.location.href = 'admin-area.php';
+                    }, 2000);
+                } else {
+                    swal(
+                        'Error!',
+                        'Usuario o password Incorrectos',
+                        'error'
+                    )
+                }
+            }
+        })
+    });
+});
