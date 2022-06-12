e90be5ee8d61d353425cf43ed2226fe9efb1e1e3
NinivePat <ninivepat@gmail.com>
2022-06-12 18:25:40 -0500

    Finalice la carpeta 80, viendo los siguientes videos que son del 12 al 20, trabaje con la parte de administradores y eventos, los añadí en la base de datos , los hice funcionales. Igualmente continúe con la carpeta 81, hasta el video 6.

---
+++ proyectoconferencias/admin/modelo-evento.php
@@ -0,0 +1,134 @@
+<?php
+include_once 'funciones/funciones.php';
+
+$titulo = $_POST['titulo_evento'];
+$categoria_id = $_POST['categoria_evento'];
+$invitado_id = $_POST['invitado'];
+//Obtener la fecha
+$fecha = $_POST['fecha_evento'];
+$fecha_formateada = date('Y-m-d', strtotime($fecha));
+
+// hora
+$hora = $_POST['hora_evento'];
+$hora_formateada = date('H:i', strtotime($hora));
+
+$id_registro = $_POST['id_registro'];
+
+if ($_POST['registro'] == 'nuevo') {
+    try{
+        $stmt = $conn->prepare("INSERT INTO eventos (nombre_evento, fecha_evento, hora_evento, id_cat_evento, id_inv) VALUES (?, ?, ?, ?, ? )");
+        $stmt->bind_param("sssii", $titulo, $fecha_formateada, $hora, $categoria_id, $invitado_id );
+        $stmt->execute();
+        $id_insertado = $stmt->insert_id;
+        if($id_registro > 0){
+            $respuesta = array(
+                'respuesta' => 'exito',
+                'id_insertado' => $id_insertado
+            );
+
+        }else{
+            $respuesta = array(
+                'respuesta' => 'error',
+
+            );
+        }
+        $stmt->close();
+        $conn->close();
+    }catch(Exception $e){
+        echo "Error: " . $e->getMessage(); 
+    }
+
+    die(json_encode($respuesta));
+}
+
+if ($_POST['registro'] == 'actualizar') {
+    
+    try{
+        $stmt = $conn->prepare('UPDATE eventos SET nombre_evento = ?, fecha_evento = ?, hora_evento = ?, id_cat_evento = ?, id_inv = ? WHERE evento_id = ? ');
+        $stmt->bind_param('sssiii', $titulo, $fecha_formateada, $hora_formateada, $categoria_id, $invitado_id, $id_registro );
+        $stmt->execute();
+        if ($stmt->affected_rows) {
+            $respuesta = array(
+                'respuesta' => 'exito',
+                'id_actualizado' => $id_registro
+            );
+        } else {
+            $respuesta = array(
+                'respuesta' => 'error'
+            );
+        }
+        $stmt->close();
+        $conn->close();
+    }catch(Exception $e){
+        $respuesta = array(
+            'respuesta' => $e->getMessage()
+        );
+    }
+
+    die(json_encode($respuesta));
+}
+
+if ($_POST['registro'] == 'eliminar') {
+    $id_borrar = $_POST['id'];
+
+    try {
+        $stmt = $conn->prepare('DELETE FROM admins WHERE id_admin = ?');
+        $stmt->bind_param('i', $id_borrar);
+        $stmt->execute();
+        if ($stmt->affected_rows) {
+            $respuesta = array(
+                'respuesta' => 'exito',
+                'id_eliminado' => $id_borrar
+            );
+        } else {
+            $respuesta = array(
+                'respuesta' => 'error',
+            );
+        }
+    } catch (Exception $e) {
+        $respuesta = array(
+            'respuesta' => $e->getMessage()
+        );
+    }
+    die(json_encode($respuesta));
+}
+
+if (isset($_POST['login-admin'])) {
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+
+    try {
+        include_once 'funciones/funciones.php';
+        $stmt = $conn->prepare("SELECT * FROM admins WHERE usuario = ?;");
+        $stmt->bind_param("s", $usuario);
+        $stmt->execute();
+        $stmt->bind_result($id_admin, $usuario_admin, $nombre_admin, $password_admin, $editado);
+        if ($stmt->affected_rows) {
+            $existe = $stmt->fetch();
+            if ($existe) {
+                if (password_verify($password, $password_admin)) {
+                    session_start();
+                    $_SESSION['usuario'] = $usuario_admin;
+                    $_SESSION['nombre'] = $nombre_admin;
+                    $respuesta = array(
+                        'respuesta' => 'exitoso',
+                        'usuario' => $nombre_admin
+                    );
+                } else {
+                    $respuesta = array(
+                        'respuesta' => 'error'
+                    );
+                }
+            } else {
+                $respuesta = array(
+                    'respuesta' => 'error'
+                );
+            }
+        }
+        $stmt->close();
+        $conn->close();
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    die(json_encode($respuesta));
+}
